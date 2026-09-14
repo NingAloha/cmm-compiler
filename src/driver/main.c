3eadad31fa2a3eae864949d95ec2ea65eec10d15
NingAloha <244903781+NingAloha@users.noreply.github.com>
2026-09-15 00:49:21 +0800

    feat: 添加初始版词法器规则

---
+++ src/driver/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+extern FILE *yyin;
+int yylex(void);
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <source.cmm>\n", argv[0]);
+        return 1;
+    }
+
+    yyin = fopen(argv[1], "r");
+    if (yyin == NULL) {
+        perror(argv[1]);
+        return 1;
+    }
+
+    yylex();
+    
+    fclose(yyin);
+    return 0;
+}
